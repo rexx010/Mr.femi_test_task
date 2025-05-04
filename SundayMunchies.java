6006aaa88ab57a6124c33f588360c4eed068ebe7
rexx010 <grexx200@gmail.com>
2025-05-04 18:50:05 +0100

    first commit

--- SundayMunchies.java
+++ SundayMunchies.java
@@ -7,10 +7,45 @@ return number;
 }
 
 
+//test 2
+public static void displayPrimeNumber(int num){
+ boolean prime = true;
+ int count = 0;
+  for(int counter = 1; counter <= num; counter++){
+  if (num % counter == 0){
+  count++;
+  }
+ }
+  if(count == 2){
+  countp++
+  System.out.println(prime+", The number is a prime number");
+} else{
+System.out.println(!prime+", The number is not a prime number");
+}
+return num;
+}
 
-//test 6
-public static int computeSumOfNumber(int number1, int number2,int number3, int number4){
-return number1 + number2 + number3 + number4;
+
+
+//test 3
+public static void displayAge(int ageYears){
+int years = ageYears;
+int months = years * 12;
+int days = years * 365;
+int minutes = 365 * 1440;
+int seconds = minutes * 60;
+System.out.println("Age is:"+years+" years, "+months+" Months, "+days+" Days, "+minutes+" Minutes, "+seconds+" Seconds");
+System.out.println();  
+}
+
+
+
+//test 4
+public static void displayDogAge(String name, int humanYear){
+String dogName = name;
+int dogYear = 7;
+int result = dogYear * humanYear;
+System.out.println("The name of the dog is "+dogName+" and the dog age is "+result);
 }
 
 
@@ -27,27 +62,25 @@ System.out.println();
 }
 
 
-
-
-public static void displayDogAge(String name, int humanYear){
-String dogName = name;
-int dogYear = 7;
-int result = dogYear * humanYear;
-System.out.println("The name of the dog is "+dogName+" and the dog age is "+result);
+//test 6
+public static int computeSumOfNumber(int number1, int number2,int number3, int number4){
+return number1 + number2 + number3 + number4;
 }
 
 
 
-public static void displayAge(int ageYears){
-int years = ageYears;
-int months = years * 12;
-int days = years * 365;
-int minutes = 365 * 1440;
-int seconds = minutes * 60;
-System.out.println("Age is:"+years+" years, "+months+" Months, "+days+" Days, "+minutes+" Minutes, "+seconds+" Seconds");
-System.out.println();
 
-  
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
