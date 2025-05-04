37cddd27c0a263f6f08e0acd7eb093aa4fb21796
rexx010 <grexx200@gmail.com>
2025-05-04 18:32:38 +0100

    first commit

---
+++ SundayMuchiesTest.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class SundayMuchiesTest{
+public static void main(String[] args){
+
+Scanner input = new Scanner(System.in);
+/*
+//test 1
+System.out.print("Enter an integer number: ");
+int num = input.nextInt();
+int a = SundayMunchies.findMultiple(num);
+System.out.println("the fifth multiple of "+num+" is "+a);
+
+
+//test 6
+System.out.print("Enter four digits: ");
+int num1 = input.nextInt();
+int num2 = input.nextInt();
+int num3 = input.nextInt();
+int num4 = input.nextInt();
+
+int sum = SundayMunchies.computeSumOfNumber(num1,num2,num3,num4);
+System.out.println("The sum is "+sum);
+
+
+
+//test 5
+System.out.print("Enter the digit your range wants to start from: ");
+int num1 = input.nextInt();
+System.out.print("Enter the digit your range wants to end: ");
+int num2 = input.nextInt();
+SundayMunchies.findMultipleof7Not5(num1, num2);
+System.out.println();
+
+
+
+//test 4
+System.out.print("Enter the dog name: ");
+String name = input.next();
+System.out.print("Enter the dog's age in human year: ");
+int age = input.nextInt();
+SundayMunchies.displayDogAge(name, age);*/
+
+
+
+//test 3
+System.out.print("Enter your age: ");
+int age = input.nextInt();
+SundayMunchies.displayAge(age);
+
+
+}
+}
