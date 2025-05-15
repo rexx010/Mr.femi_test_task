4c2576f3f2073de338fdab828456df98af37433c
rexx010 <grexx200@gmail.com>
2025-05-15 22:06:13 +0100

    created an array class

---
+++ ArrayRange.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ArrayRange{
+public static void main(String[] args){
+
+
+int arr[] = {5, 6, 10, 15,20};
+
+
+System.out.println(Arrays.toString(ArrayRangeTest.length(arr)));
+
+
+
+
+}
+}
