74350f4e0f016420a360402bc6916c3a4bf1059e
rexx010 <grexx200@gmail.com>
2025-05-21 12:10:44 +0100

    updated the atm test

--- TestDrivenDevelopment/AutomatedTellerMachineTest.java
+++ TestDrivenDevelopment/AutomatedTellerMachineTest.java
@@ -1,37 +1,51 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AutomatedTellerMachineTest{
+private AutomatedTellerMachine atm = new AutomatedTellerMachine();
+private Account johnsAccount;
+private Account jamesAccount;
+private Account bolajisAccount;
+
+
+	@BeforeEach
+	public void setUp(){
+
+		System.out.print("Setting up the test");
+		//pre-condition(s)
+		johnsAccount = new Account("John", "0123456789", 50.00);
+
+		jamesAccount = new Account("James", "9876543210", 50000.00);
+	
+
+		bolajisAccount = new Account("Bolaji", "1023456789", 500.00);
+	
+
+		Account [] accounts = {johnsAccount, jamesAccount, bolajisAccount};
+
+		
+		atm.setAccount(accounts);
+	}
+
 
 	@Test
 	public void testCanWithdrawFunds(){
-		//pre-condition(s)
-		Account johnsAccount = new Account("John", "0123456789", 50.00);
-
-		Account jamesAccount = new Account("James", "9876543210", 50000.00);
-	
-
-		Account bolajisAccount = new Account("Bolaji", "1023456789", 500.00);
-	
-
-Account [] accounts = {johnsAccount, jamesAccount, bolajisAccount};
-
-		AutomatedTellerMachine atm = new AutomatedTellerMachine();
-		
-		atm.setAccount(accounts);
-
 		//action
-
 		atm.withdraw(jamesAccount.getAccountNumber(), 20000);
-
 		//check(s)
-
 		assertEquals(30000, jamesAccount.getBalance());
 		
 }
 
 
+	@Test
+	public void testWithdrawNegativeAmountThrowsException(){
+		atm.wihdraw(accounts[1].getAccountNumber(), -30_000); 
+		assertThrows();
 
+
+}
 
 
 
