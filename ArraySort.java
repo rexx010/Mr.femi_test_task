9c20399a66480b6c077eb4d36134dc925f25f15c
rexx010 <grexx200@gmail.com>
2025-05-21 12:12:42 +0100

    sorting of array in ascending order

---
+++ ArraySort.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class ArraySort{
+public static void main(String[] args){
+
+int []sort = {9, 7, 1, 5, 2};
+
+System.out.print(Arrays.toString(ArraySortFunction.sort(sort)));
+
+
+}
+}
