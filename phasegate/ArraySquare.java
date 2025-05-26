0cad6b3a7d53f43b96b0e48d400d50e9c33ae1e5
rexx010 <grexx200@gmail.com>
2025-05-26 17:21:39 +0100

    created square of array

---
+++ phasegate/ArraySquare.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class ArraySquare{
+public static void main(String[] args){
+
+int []sort = {9, 2, 7, 0, 5};
+
+System.out.print(Arrays.toString(ArraySquareFunction.sort(sort)));
+
+
+}
+}
