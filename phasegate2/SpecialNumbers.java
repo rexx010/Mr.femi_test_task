0a894eb87efa9f534b546fe04d042f595b10e102
rexx010 <grexx200@gmail.com>
2025-05-28 20:41:29 +0100

    first commit

---
+++ phasegate2/SpecialNumbers.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class SpecialNumbers{
+public static void main(String[] args){
+
+int[] number = {1, 2, 3, 2};
+System.out.print(SpecialNumbersFunction.add(number));
+
+
+}
+}
