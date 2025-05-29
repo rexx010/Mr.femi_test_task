9dcdaf52a38c756508f84a73fe828e14f96a41ba
rexx010 <grexx200@gmail.com>
2025-05-29 16:50:21 +0100

    first commit

---
+++ phasegate3/UnsortedList.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class UnsortedList{
+public static void main(String[] args){
+
+int[] num1 = {3, 1, 5};
+int[] num2 = {8, 6, 4};
+System.out.print(Arrays.toString(UnsortedListFunction.sorted(num1, num2)));
+}
+
+}
