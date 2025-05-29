df0d65dcfe4c5629f567591201dea8f5ed485600
rexx010 <grexx200@gmail.com>
2025-05-29 16:50:47 +0100

    first commit

---
+++ phasegate3/UnsortedListFunctionTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UnsortedListFunctionTest{
+
+	@Test
+	public void Functionexit{
+
+	int[] number = {3, 1, 5, 8, 6, 4};
+
+	UnsortedListFunction.sorted(number);
+	}
+
+
+
+}
+
+}
