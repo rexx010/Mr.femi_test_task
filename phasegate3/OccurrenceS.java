660d505fc49de5fb7a41db8d8e632c0ba2f95c92
rexx010 <grexx200@gmail.com>
2025-05-29 16:49:46 +0100

    first commit

---
+++ phasegate3/OccurrenceS.java
@@ -0,0 +1,8 @@
+public class OccurrenceS{
+public static void main(String[] args){
+
+String word = "Hello World";
+char letter = 'o';
+System.out.print(OccurrenceSFunction.number(word, letter));
+}
+}
