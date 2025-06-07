69cafbb25959538dd8c4ab0612477839c9c1d14e
rexx010 <grexx200@gmail.com>
2025-06-07 01:29:02 +0100

    updated the app

--- phasegateOne/MentrualApp.java
+++ phasegateOne/MentrualApp.java
@@ -12,9 +12,6 @@ Welcome to Phasegate Menstrual cycle calculator...
 press any of the following to perform an action
 
 1: to check calculate the date of your menstrual cycle.
-2: to know your flow date.
-3: to know your safe period.
-4: to know your ovulation date.
 0: to exit the app.
 """;
 
@@ -28,27 +25,41 @@ case 1:
 	int year = input.nextInt();
 	int month = input.nextInt();
 	int day = input.nextInt();
+	System.out.println();
+
 	System.out.println("How many days is your cycle?: ");
 	int cycle = input.nextInt();
+	System.out.println();
+
+
 	LocalDate status = MentrualAppFunction.begin(year, month, day);
 	LocalDate end = MentrualAppFunction.finish(year, month, day, cycle);
+	String flow = MentrualAppFunction.flowDate(year, month, day);
 	String ovu = MentrualAppFunction.ovulation(year, month, day, cycle);
+	String fertile = MentrualAppFunction.fertileLength(year, month, day, cycle);
+	String safety = MentrualAppFunction.safeperiod(year, month, day, cycle);
+	System.out.println();
+
 	System.out.println("Your cycle starts on the "+status);
+	System.out.println();
+
 	System.out.println("and your cycle ends on the "+end);
+	System.out.println();
+
+	System.out.println(flow);
+	System.out.println();
+
+
 	System.out.println(ovu);
+	System.out.println();
+
+	System.out.println(fertile);
+	System.out.println();
+
+	System.out.println(safety);
+	System.out.println(); 
 	break;
-case 2:
-	//String ovu = MentrualAppFunction.ovulation;
-	//System.out.println(ovu);
-	break;
-case 3:
-	System.out.println("Welcome to your calculater \n Enter your start date: ");
-	int start = input.nextInt();
-	break;
-case 4:
-	System.out.println("Welcome to your calculater \n Enter your start date: ");
-	start = input.nextInt();
-	break;
+
 case 0:
 	condition = false;
 	break;
