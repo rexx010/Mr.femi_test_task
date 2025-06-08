668f4e1a73ab6b6a804ab909f21ef0d20ccc2a6d
rexx010 <grexx200@gmail.com>
2025-06-08 13:56:18 +0100

    updated

--- phasegateOne/CheckOutApp.java
+++ phasegateOne/CheckOutApp.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class CheckOutApp{
 public static void main(String[] args){
 
+Scanner input = new Scanner(System.in);
+
+System.out.println("What is the customer's name?");
+String name = input.next();
+
+System.out.println("What did the user buy?");
+String product = input.next();
+
+System.out.println("How many pieces?");
+int item = input.next();
+
+System.out.println("How much per unit?");
+int price = input.next();
+
+System.out.println("Add more items?");
+int item = input.next();
+
+System.out.println("What is your name?");
+String cashierName = input.next();
+
+System.out.println("How much discount will he/she get?");
+int item = input.next();
 
 }
 
